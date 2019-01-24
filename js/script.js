ac4db9c784f0c0d593a606fa25fdb68ae4a91f2e
MarieFocke <mariefocke@gmail.com>
2019-01-23 18:04:42 -0800

    Start of the project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 $(function() {
   console.log("Welcome to Instanews!");
 });
+
+$(".button").one("click", function() {
+    $.ajax({
+      method: 'GET',
+       url:
+      ''})
+      .done(function(data) {
+          $('.results').empty();
+          $.each(data.weather, function(key,value){
+            const iconurl="<img src=http://openweathermap.org/img/w/" + value.icon + ".png">"
+            
+            $(".results").append("<p>"+ iconurl +"</p>"),
+            $(".results").append("<p class='main'>"+ value.main +"</p>"),
+            $(".results").append("<p class='description'>"+ value.description +"</p>");
+            console.log(value);
+            });    
+          })
+            .fail(function() {
+             $(".results").append("<p>"+ "Sorry, there was a problem, please try again." + "</p>")
+              })
+  });
